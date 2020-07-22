761bf7375ebb25d5e14092980cf7f2db381e0927
Nick Misiiuk <nickmisiiuk@MacBook-Pro-Volodymyr.local>
2020-07-22 13:47:48 +0300

    First completion

--- db_mysql.py
+++ db_mysql.py
@@ -4,27 +4,27 @@ from sqlalchemy import create_engine
 import pymysql
 
 # Create a decorator that connects to the DB and closes the connection once the function is done
-def ensure_connection(func):
+def start_connection():
+    # conn = connection.connect(user='sql7356052', password='XrZadZZzmv',
+    #                           host='sql7.freemysqlhosting.net',
+    #                           database='sql7356052')#, charset='utf8')
+    conn = connection.connect(user='nick', password='usalud35',
+                              host='mysql-11179-0.cloudclusters.net', port=11179,
+                              database='orders')
+    cursor = conn.cursor()
+    return conn, cursor
     
-    def inner(*args, **kwargs):
-        with connection.connect(user='sql7356052', password='XrZadZZzmv',
-                              host='sql7.freemysqlhosting.net',
-                              database='sql7356052', charset='utf8') as conn:
-            kwargs['conn'] = conn
-            res = func(*args, **kwargs)
-        return res
-
-    return inner
-
-@ensure_connection
-def init_db(conn, force=False):
+def stop_connection(conn, cursor):
+    cursor.close()
+    conn.close()
     
+def init_db(force=False):
+    conn, cursor = start_connection()
     ''' Check that the required tables exist, else create them'''
     ''':param force: Create tables again'''
-    cursor = conn.cursor()
     if force:
         cursor.execute('DROP TABLE IF EXISTS order_basket')
-        cursor.execute('DROP TABLE IF EXISTS addresses')
+        cursor.execute('DROP TABLE IF EXISTS client_info')
     
     # Create a table in DB to store a user's order basket
     cursor.execute('''CREATE TABLE IF NOT EXISTS order_basket (
@@ -32,37 +32,36 @@ def init_db(conn, force=False):
                         user_id INTEGER NOT NULL,
                         name TEXT NOT NULL,
                         quantity INTEGER NOT NULL,
-                        price FLOAT NOT NULL
-                        )'''
+                        price FLOAT NOT NULL,
+                        ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                        ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci '''
                         )
     # Create a table in DB to store user addresses
-    cursor.execute('''CREATE TABLE IF NOT EXISTS addresses (
+    cursor.execute('''CREATE TABLE IF NOT EXISTS client_info (
                         id INTEGER AUTO_INCREMENT PRIMARY KEY REFERENCES order_basket(id),
                         user_id INTEGER NOT NULL,
-                        address TEXT NOT NULL
-                        )'''
+                        latitude TEXT,
+                        longitude TEXT, 
+                        phone_number TEXT,
+                        username TEXT
+                        ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci '''
                         )
     conn.commit()
-    cursor.close()
-    conn.close()
+    stop_connection(conn, cursor)
     
-@ensure_connection   
-def add_order(conn, user_id, name, quantity, price):
+def add_order(user_id, name, quantity, price):
     ''' Добавляет единицу товара в таблицу'''
-    cursor = conn.cursor()
+    conn, cursor = start_connection()
     cursor.execute('INSERT INTO order_basket (user_id, name, quantity, price) VALUES (%s, %s, %s, %s)', (user_id, name, quantity, price))
     conn.commit()
-    cursor.close()
-    conn.close()
+    stop_connection(conn, cursor)
+
     
-@ensure_connection
-def list_order(conn, user_id):
+def list_order(user_id):
     '''List the basket to the user'''
-    cursor = conn.cursor()
-    cursor.execute('SELECT name, SUM(price) FROM order_basket WHERE user_id = %s GROUP BY price', (user_id, ))
+    conn, cursor = start_connection()
+    cursor.execute('SELECT name, SUM(quantity), SUM(price) FROM order_basket WHERE user_id = %s GROUP BY price, name, quantity', (user_id, ))
     list_of_tuples = cursor.fetchall()
-    cursor.close()
-    conn.close()
     
     # Unpack a list of tuples and add each tuple into a list
     res_list = []
@@ -70,6 +69,7 @@ def list_order(conn, user_id):
         tup = list(tup)
         res_list.append(tup)
         yield res_list[count]
+    stop_connection(conn, cursor)
 
 # Now transform the Excel price list into a table in the database
 # def excel_connect():
@@ -84,17 +84,24 @@ def list_order(conn, user_id):
 #     prices.to_sql('prices', con=conn, if_exists='replace', index=False)
 #     conn.close()
 
-@ensure_connection
-def access_price_list(conn, name):
+def access_price_list(name):
     '''Access a required position in the price list'''
-    cursor = conn.cursor()
+    conn, cursor = start_connection()
     cursor.execute('SELECT name, price FROM prices WHERE name = %s', (name, ))
     tup = cursor.fetchone()
-    cursor.close()
-    conn.close()
+    stop_connection(conn, cursor)
     return tup # Returns a tuple of (name, price) type
+
+def write_location(user_id, lat, long):
+    conn, cursor = start_connection()
+    cursor.execute('INSERT INTO client_info (user_id, latitude, longitude) VALUES (%s, %s, %s)', (user_id, lat, long))
+    conn.commit()
+    stop_connection(conn, cursor)
     
-'''Need this code to get all lists in the generator object
-   returned by list_order()'''
-# for lst in list_order(user_id=1488):
-#     print(lst)
+def write_username(user_id, username):
+    conn, cursor = start_connection()
+    cursor.execute('INSERT INTO client_info (user_id, username) VALUES (%s, %s)', (user_id, username))
+    conn.commit()
+    stop_connection(conn, cursor)
+
+# Function that deletes user's order from DB after pressed "DONE" ??
